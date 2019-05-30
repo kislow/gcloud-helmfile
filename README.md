b59dc3ac040757c2bf63d8c59efe063e6e776cd3
Bastian Eicher <bastian.eicher@axoom.com>
2019-05-30 13:41:08 +0200

    Improves handling of environment variables

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ You can configure it by setting the following environment variables:
 | `GCP_PROJECT`                    | **required**   | The Google Cloud project containing the Kubernetes cluster.                                                                         |
 | `CLUSTER_NAME`                   | **required**   | The name of the Kubernetes cluster.                                                                                                 |
 | `CLUSTER_REGION`                 | `europe-west3` | The region the Kubernetes cluster is deployed to.                                                                                   |
-| `CI_PROJECT_DIR`                 | **required**   | The directory containing the `helmfile.yaml`.                                                                                       |
+| `CI_PROJECT_DIR`                 | `.`            | The directory containing the `helmfile.yaml`.                                                                                       |
 | `HELMFILE_OPERATION`             | `sync`         | The command to pass to `helmfile`.                                                                                                  |
 | `GitVersion_NuGetVersion`        | -              | If specified, patches this as `version` and `appVersion` into all `Chart.yaml` files in subdirectories of `$CI_PROJECT_DIR/charts`. |
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 set -e
 
-# Sanity checks
-if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
-  echo Environment variable GOOGLE_APPLICATION_CREDENTIALS not set!
-  exit 1
-fi
-if [ -z "$GCP_PROJECT" ]; then
-  echo Environment variable GCP_PROJECT not set!
-  exit 1
-fi
-if [ -z "$CLUSTER_NAME" ]; then
-  echo Environment variable CLUSTER_NAME not set!
-  exit 1
-fi
-if [ -z "$CI_PROJECT_DIR" ]; then
-  echo Environment variable CI_PROJECT_DIR not set!
-  exit 1
+# Connect to Kubernetes cluster
+if [ ! -f ~/.kube/config ]; then
+  if [ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]; then
+    echo "Environment variable GOOGLE_APPLICATION_CREDENTIALS not set!"
+    exit 1
+  fi
+  if [ -z "$GCP_PROJECT" ]; then
+    echo "Environment variable GCP_PROJECT not set!"
+    exit 1
+  fi
+  if [ -z "$CLUSTER_NAME" ]; then
+    echo "Environment variable CLUSTER_NAME not set!"
+    exit 1
+  fi
+  gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
+  gcloud beta container clusters get-credentials "$CLUSTER_NAME" --region "${CLUSTER_REGION:-europe-west3}" --project "$GCP_PROJECT"
 fi
 
-# Connect to Kubernetes Cluster
-gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
-gcloud beta container clusters get-credentials $CLUSTER_NAME --region ${CLUSTER_REGION:-europe-west3} --project $GCP_PROJECT
-
-cd $CI_PROJECT_DIR
+# Set working directory
+cd "${CI_PROJECT_DIR:-.}"
 
 # Patch version number in charts
 shopt -s nullglob

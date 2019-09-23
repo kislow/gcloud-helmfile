6eb47b9c91eeea1c726c52a87975014c5e24a7ee
Bastian Eicher <bastian.eicher@axoom.com>
2019-09-23 11:00:57 +0200

    Escaped database name

--- database-init.sh
+++ database-init.sh
@@ -19,8 +19,8 @@ kubectl port-forward -n cloud-sql svc/cloud-sql 5432 &
 sleep 2 # Short delay to allow for connection to be established
 
 # Try to create database and username; ignore failure in case they already exist
-psql --host=127.0.0.1 --user=postgres --command="CREATE DATABASE $DATABASE_NAME;" || true
-psql --host=127.0.0.1 --user=postgres --command="CREATE USER $DATABASE_NAME;" || true
+psql --host=127.0.0.1 --user postgres --command="CREATE DATABASE \"$DATABASE_NAME\";" || true
+psql --host=127.0.0.1 --user postgres --command="CREATE USER \"$DATABASE_NAME\";" || true
 
 # Update user password and privileges
-psql --host=127.0.0.1 --user=postgres --command="ALTER USER $DATABASE_NAME WITH PASSWORD '$DATABASE_PASSWORD'; GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME TO $DATABASE_NAME;"
+psql --host=127.0.0.1 --user=postgres --command="ALTER USER \"$DATABASE_NAME\" WITH PASSWORD '$DATABASE_PASSWORD'; GRANT ALL PRIVILEGES ON DATABASE \"$DATABASE_NAME\" TO \"$DATABASE_NAME\";"
